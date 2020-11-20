b879d35b7932658014f9b86c21850cbac07bf593
Orion <gmd42@pitt.edu>
2020-11-19 21:15:50 -0500

    Blinky

--- src/contact_sensor.py
+++ src/contact_sensor.py
@@ -2,7 +2,7 @@ class contact_sensor():
 	def __init__(self, GPIO, sensor_pin):
 		self.GPIO = GPIO
 		self.sensor_pin = sensor_pin
-		self.validateList = [0, 0, 0, 1, 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0] # fill in with expected readings of sensor for every step
+		self.validateList = [0, 0, 0, 1, 1, 1, 1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0] # fill in with expected readings of sensor for every step
 
 	def validateStep(self, stepNumber):
 			assert self.GPIO.input(self.sensor_pin) == self.validateList[stepNumber]
